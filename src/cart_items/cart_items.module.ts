e1cc95389a980e5f05dde7aa0b61ce7b0d4cd450
sabidjanovv <sabidjanov@gmail.com>
2025-01-14 15:34:45 +0500

    Cart and CartItems

--- src/cart_items/cart_items.module.ts
+++ src/cart_items/cart_items.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CartItemsService } from './cart_items.service';
 import { CartItemsController } from './cart_items.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CartItems } from './models/cart_item.model';
+import { Cart } from '../cart/models/cart.model';
+import { Product } from '../product/models/product.model';
 
 @Module({
+  imports:[SequelizeModule.forFeature([CartItems, Cart, Product])],
   controllers: [CartItemsController],
   providers: [CartItemsService],
 })
--- src/cart_items/models/cart_item.model.ts
+++ src/cart_items/models/cart_item.model.ts
@@ -1,1 +1,64 @@
-export class CartItem {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Column,
+  DataType,
+  Model,
+  Table,
+  HasMany,
+  ForeignKey,
+  BelongsTo,
+} from 'sequelize-typescript';
+import { Client } from '../../client/models/client.model';
+import { Cart } from '../../cart/models/cart.model';
+import { Product } from '../../product/models/product.model';
+interface ICartItemsAttr {
+  clientId: number;
+  createdAt: string;
+}
+
+@Table({ tableName: 'cart_items' })
+export class CartItems extends Model<CartItems, ICartItemsAttr> {
+  @ApiProperty({ example: 1, description: 'Unique Cart ID' })
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  id: number;
+
+  @ApiProperty({
+    example: 1,
+    description: 'Cart ID',
+  })
+  @ForeignKey(() => Cart)
+  @Column({
+    type: DataType.BIGINT,
+    allowNull: false,
+  })
+  cartId: number;
+  @BelongsTo(() => Cart)
+  cart: Cart;
+
+  @ApiProperty({
+    example: 1,
+    description: 'Product ID',
+  })
+  @ForeignKey(() => Product)
+  @Column({
+    type: DataType.BIGINT,
+    allowNull: false,
+  })
+  productId: number;
+  @BelongsTo(() => Product)
+  product: Product;
+
+  @ApiProperty({
+    example: 1,
+    description: 'Quantity cart items',
+  })
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  quantity: number;
+}
