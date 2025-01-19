d9c752da8966c120913f651b99dffd7d772c2dc6
sabidjanovv <sabidjanov@gmail.com>
2025-01-19 11:32:48 +0500

    HBS(otp) improved

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,17 +1,19 @@
-import { Injectable } from "@nestjs/common";
-import { MailerService } from "@nestjs-modules/mailer";
-import { Admin } from "../admin/models/admin.model";
+import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+import { Admin } from '../admin/models/admin.model';
 // import { User } from "../user/models/user.model";
 
 @Injectable()
 export class MailService {
-  constructor(private readonly mailerService: MailerService) {}
+  constructor(
+    private readonly mailerService: MailerService,
+  ) {}
 
   async sendMail(admin: Admin) {
     const url = `${process.env.API_URL}:${process.env.PORT}/api/auth/activate/${admin.activation_link}`;
     await this.mailerService.sendMail({
       to: admin.email,
-      subject: "Welcome to Mebel House",
+      subject: 'Welcome to Mebel House',
       template: './confirm',
       context: {
         full_name: admin.full_name,
