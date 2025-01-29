f9aae741723828879c74106654bafa33f1898324
sabidjanovv <sabidjanov@gmail.com>
2025-01-29 11:26:14 +0500

    Port fixed

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,13 @@ console.log(join(__dirname, '..', 'uploads'));
         origin: (origin, callback) => {
           const allowedOrigins = [
             'http://localhost:5173',
+            'http://localhost:5174',
+            'http://localhost:5175',
+            'http://localhost:5176',
+            'http://localhost:5177',
+            'http://localhost:5178',
+            'http://localhost:5179',
+            'http://localhost:5180',
             'http://localhost:3001',
             'http://localhost:3011',
             'http://167.71.195.218:3001',
